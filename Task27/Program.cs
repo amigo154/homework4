5dd7edc00fa5f88e8cfe2581457e85859950fa32
Aleksandr Strabykin <strabykin-23@mail.ru>
2023-01-15 15:34:18 +0700

    task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("vvedite chislo: "); // 
+int num = int.Parse(Console.ReadLine());
+
+int GetCountSum(int number)  // функция
+{
+    int sum = 0;
+    int c = 0;
+    while (number > 0)
+    {
+        c = number % 10;
+        sum += c;
+        number /= 10;
+    }
+    return sum;
+}
+
+int function = GetCountSum(num); // вызвали функцию
+Console.WriteLine($"количество цифр в числе {num} получилось {function}");
